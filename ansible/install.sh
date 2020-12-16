aaefb0ae03fc8434d893c3334a76df38ab15038d
João de Faria Jr <jdfaria21@gmail.com>
2020-12-16 18:27:49 -0300

    update

--- ansible/install.sh
+++ ansible/install.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
+echo "
 # -------------------------------
 # APT GET UPDATE
-# -------------------------------
+# -------------------------------"
 echo "UPDATE"
-apt-get update
+apt-get update && apt-get install unzip -y
+
+echo "
 # -------------------------------
 # INSTALL DOCKER
-# -------------------------------
-echo "INSTALL DOCKER"
+# -------------------------------"
 curl -fsSL https://get.docker.com | bash
+
+echo "
+# -------------------------------
+# INSTALL AWS cli
+# -------------------------------"
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
